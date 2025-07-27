d3613b5e7048d3b07af0596828c87f3e437e0ab0
Synxdicate <thanadondevsec@gmail.com>
2025-07-28 00:50:58 +0700

    change port from 3000 to 80

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('http://localhost:3000');
+server.listen(80, () => {
+    console.log('http://localhost');
 });
